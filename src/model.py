7826e0f82e27cc92875daf800a1f1d5095ab43d1
Victor Reyes <vmrespinoza@gmail.com>
2018-08-14 20:28:46 -0700

    updated the new model code

--- src/model.py
+++ src/model.py
@@ -14,6 +14,9 @@ from ge_data_parser import *
 class ge_predictor:
 
     def __init__(self):
+        """
+        Initializes the model with empty values.
+        """
         self.model = None
         self.training_features = None
         self.training_targets = None
@@ -21,8 +24,12 @@ class ge_predictor:
         self.testing_targets = None
         self.data_set = False
 
-    def set_data(self, training_features ,training_targets, testing_features, \
+    def set_data(self, training_features, training_targets, testing_features, \
                  testing_targets):
+        """
+        Input: training and testing features in the form of dataframes.
+        Set the data for model and store it.
+        """
         self.training_features = training_features
         self.training_targets = training_targets
         self.testing_features = testing_features
@@ -31,6 +38,9 @@ class ge_predictor:
         self.data_length = len(training_features)
 
     def setup_model(self):
+        """
+        Sets up, compiles and stores a predefined model.
+        """
         if not self.data_set:
             print("[!!] You do not have any data set into the model. \
                    Please run set_data before setting up them model!")
@@ -41,7 +51,7 @@ class ge_predictor:
         _model.add(Dense(self.num_features, input_dim=self.num_features))
         _model.add(Activation('relu'))
         # https://stats.stackexchange.com/questions/181/how-to-choose-the-number-of-hidden-layers-and-nodes-in-a-feedforward-neural-netw
-        # used formula from comment 2 
+        # used formula from comment 2
         alpha = 5 # usually 2-10
         hidden_layer_neurons = int(self.data_length/(alpha*(self.num_features+1)))
         print("[!] The model has %i neurons in its hidden layer." % hidden_layer_neurons)
@@ -51,3 +61,5 @@ class ge_predictor:
 
         _model.compile(optimizer='Adam',
                     loss='mse')
+
+        self.model = _model
